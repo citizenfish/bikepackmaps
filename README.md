d931a65175a7a1025d35caa8696a42eaa04bff44
citizenfish <dave@nautoguide.com>
2023-10-04 12:27:19 +0100

    First tests of output Styles added

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 
 Reboot of my old project to create Garmin maps suitable for Bikepackers
 
+Current test command line
+
+```shell
+java -jar ./bin/mkgmap/mkgmap.jar --style-file=./style/mkgmap_styles --style=VMD_style --input-file=./prototyping/data/brixham.osm --gmapsupp --output-dir=./prototyping/output
+```
--- prototyping/data/make_brixham_extract.sh
+++ prototyping/data/make_brixham_extract.sh
@@ -1,3 +1,5 @@
 ogr2ogr -f gpkg -spat 291325 55730 294385 56929 ./prototyping/data/brixham.gpkg  /Volumes/Extreme\ Pro/mapping_data/zoomstack/OS_Open_Zoomstack.gpkg
 
 ogr2osm ./prototyping/data/brixham.gpkg -o ./prototyping/data/brixham.osm
+
+java -jar ./bin/mkgmap/mkgmap.jar --style-file=./style/mkgmap_styles --style=VMD_style --input-file=./prototyping/data/brixham.osm --gmapsupp --output-dir=./prototyping/output

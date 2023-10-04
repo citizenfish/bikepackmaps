a65fa6ca7ae5d0f20dc6331665a559f309b64ccc
citizenfish <dave@nautoguide.com>
2023-10-04 14:35:31 +0100

    First tests of output Styles added

--- prototyping/data/make_brixham_extract.sh
+++ prototyping/data/make_brixham_extract.sh
@@ -2,4 +2,7 @@ ogr2ogr -f gpkg -spat 291325 55730 294385 56929 ./prototyping/data/brixham.gpkg 
 
 ogr2osm ./prototyping/data/brixham.gpkg -o ./prototyping/data/brixham.osm
 
-java -jar ./bin/mkgmap/mkgmap.jar --style-file=./style/mkgmap_styles --style=VMD_style --input-file=./prototyping/data/brixham.osm --gmapsupp --output-dir=./prototyping/output
+java -jar ./bin/splitter/splitter.jar --output-dir=./prototyping/output ./prototyping/data/brixham.osm
+
+java -jar ./bin/mkgmap/mkgmap.jar --style-file=./style/mkgmap_styles --style=VMD_style -c ./prototyping/output/template.args --gmapsupp --output-dir=./prototyping/output/Garmin
+
